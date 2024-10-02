15b8aa2d847af4506854db723d87d30330ae66c1
jeongseonwoong <tjsdnd1219@naver.com>
2024-10-02 23:34:13 +0900

    foreach 함수의 뒤에는 iterator를 제공하는 collection framework들만 들어올 수 있음. String을 collection framework인 Array로 변환해주기 위해 string.toCharArray()를 해준다.

--- java/src/string/ex1.java
+++ java/src/string/ex1.java
@@ -7,6 +7,8 @@ public class ex1 {
     public static int solution(String str, char c){
         int count=0;
         str = str.toUpperCase();
+        /*foreach 함수의 뒤에는 iterator를 제공하는 collection framework들만 들어올 수 있음.
+         String을 collection framework인 Array로 변환해주기 위해 string.toCharArray()를 해준다.*/
         for(char x: str.toCharArray()){
             if(x==c){
                 count++;
