aae43c6d49ae1a03d86ed9854cbe630d0ca119b4
jeongseonwoong <tjsdnd1219@naver.com>
2024-11-17 20:36:44 +0900

    Erastotenes

--- java/src/Array/ex5/Eratostenes.java
+++ java/src/Array/ex5/Eratostenes.java
@@ -1,2 +1,25 @@
-package array.ex5;public class Eratostenes {
+package array.ex5;
+
+import java.util.Scanner;
+
+public class Eratostenes {
+    public static boolean isPrime(int n) {
+        for (int i = 2; i < n; i++) {
+            if(n % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static void solution(int num){
+        int count = 0;
+        for(int i = 2; i < num; i++){
+            if(isPrime(i)) count++;
+        }
+        System.out.println(count);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        solution(sc.nextInt());
+    }
 }
